476368f0350ea7a6e98213901e2ce70245521dec
causeloven <thigo.pro@gmail.com>
2026-04-10 09:36:21 +0300

    firsty

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args){
+
+
+    }
+
+    public boolean Solution(int[] gas, int[] cost) {
+
+    }
+}
