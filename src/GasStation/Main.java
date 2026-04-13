79a14c7719c5b8491c6c82c19441ec71ded81179
causeloven <thigo.pro@gmail.com>
2026-04-13 13:05:08 +0300

    sortedSquares

--- src/GasStation/Main.java
+++ src/GasStation/Main.java
@@ -1,53 +1,32 @@
 package GasStation;
 
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
 
 public class Main {
     public static void main(String[] args) {
-        MinStack minStack = new MinStack();
-
-        minStack.push(2);
-
+        int[] nums = {-4,-1,0,3,10};
+        Solution solution = new Solution();
+        System.out.println(solution.sortedSquares(nums));
     }
 }
 
-class MinStack {
-    Deque<Integer> mainStack = new ArrayDeque<>();
-    Deque<Integer> minStack = new ArrayDeque<>();
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
 
-    public MinStack() {
+        int left = 0;
+        int right = nums.length - 1;
 
-    }
-
-    public void push(int val) {
-        mainStack.push(val);
-        if(minStack.isEmpty()){
-            minStack.push(val);
-        }else{
-            int current = minStack.peek();
-            if(current > val){
-                minStack.push(val);
-            }else {
-                minStack.push(current);
+        for(int i = nums.length - 1; i >= 0 ; i--){
+            if(Math.pow(nums[left], 2 ) >Math.pow(nums[right], 2 )){
+                result[i] = (int) Math.pow(nums[left], 2 );
+                left++;
+            } else{
+                result[i] = (int) Math.pow(nums[right], 2 );
+                right--;
             }
         }
-    }
 
-    public void pop() {
-        mainStack.pop();
-        minStack.pop();
-    }
-
-    public int top() {
-        return mainStack.peek();
-    }
-
-    public int getMin() {
-        return minStack.peek();
+        return result;
     }
 }
-
