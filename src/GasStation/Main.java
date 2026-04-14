c0f1330e35a2ef9ae7620b0489723265f5c943ec
causeloven <thigo.pro@gmail.com>
2026-04-14 21:04:52 +0300

    PascalTriangle

--- src/GasStation/Main.java
+++ src/GasStation/Main.java
@@ -1,67 +1,15 @@
 package GasStation;
 
-import java.util.*;
+import List.ArrayList.PascalTriangle;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "ADOBECODEBANC", t = "ABC";
-        Solution solution = new Solution();
-        System.out.println(solution.minWindow(s,t));
+        PascalTriangle solution = new PascalTriangle();
+
+        int rows = 5;
+
+        solution.generate(5);
+
     }
 }
 
-class Solution {
-    public String minWindow(String s, String t) {
-        int left = 0;
-        int right = 0;
-        int minLength = Integer.MAX_VALUE;
-        int startIndex = 0;
-
-        Map<Character, Integer> targetMap = new HashMap<>();
-        for(char c : t.toCharArray()){
-            targetMap.put(c, targetMap.getOrDefault(c,0)+1);//этот метод подсмотрел в инете
-        }
-        int required = targetMap.size();
-        int formed = 0;
-
-        Map<Character, Integer> windowMap = new HashMap<>();
-
-        while(right < s.length()){
-            char c = s.charAt(right);
-
-            windowMap.put(c, windowMap.getOrDefault(c,0)+1);
-
-            if(targetMap.containsKey(c) && targetMap.get(c).equals(windowMap.get(c))){
-                formed++;
-            }
-
-            while(left<=right && formed == required){
-
-                int currentLength = right - left + 1;
-                if(currentLength < minLength){
-                    minLength = currentLength;
-                    startIndex = left;
-                }
-
-                char leftChar = s.charAt(left);
-
-                windowMap.put(leftChar, windowMap.get(leftChar)-1);
-
-                if (targetMap.containsKey(leftChar) && windowMap.get(leftChar) < targetMap.get(leftChar)){
-                    formed--;
-                }
-
-                left++;
-
-            }
-
-            right++;
-        }
-
-        if(minLength == Integer.MAX_VALUE){
-            return "";
-        }else {
-            return s.substring(startIndex, startIndex+minLength);
-        }
-    }
-}
---
+++ src/List/ArrayList/PascalTriangle.java
@@ -0,0 +1,34 @@
+package List.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++){
+            List<Integer> row = new ArrayList<>();
+
+            if (i == 0) {
+                row.add(1);
+                triangle.add(row);
+                continue;
+            }
+
+            List<Integer> prevRow = triangle.get(i - 1);
+
+            row.add(1);
+
+            for(int j = 1; j < i; j++){
+                row.add(prevRow.get(j) + prevRow.get(j-1));
+            }
+
+            row.add(1);
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+}
