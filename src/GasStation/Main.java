7ff496f2265fe07ad850fbb869c5c7d3520ff940
causeloven <thigo.pro@gmail.com>
2026-04-16 15:10:41 +0300

    ValidAnagram242

--- src/GasStation/Main.java
+++ src/GasStation/Main.java
@@ -1,16 +1,15 @@
 package GasStation;
 
-import List.ArrayList.PascalTriangle;
-import Set.ContainsDuplicate217;
-
 public class Main {
     public static void main(String[] args) {
-        ContainsDuplicate217 containsDuplicate217 = new ContainsDuplicate217();
+        ValidAnagram242 validAnagram242 = new ValidAnagram242();
 
-        int[] nums = new int[]{1,2,3,3};
+        String s = "anagram";
+        String t = "nagaram";
 
-        System.out.println(containsDuplicate217.containsDuplicate(nums));
+        validAnagram242.isAnagram(s,t);
+
+        System.out.println();
 
     }
 }
-
---
+++ src/HashMap/ValidAnagram242.java
@@ -0,0 +1,37 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class ValidAnagram242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> sMap = new HashMap<>();
+
+
+        // здесь я думал взять первую строку и посчитать кол во букв в ней
+        //их частотность
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (int j = 0; j < t.length(); j++) {
+            char c = t.charAt(j);
+
+            if (!sMap.containsKey(c)) {
+                return false;
+            }
+
+            sMap.put(c, sMap.getOrDefault(c, 0) - 1);
+
+            if (sMap.get(c) < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
