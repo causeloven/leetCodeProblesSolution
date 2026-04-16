335737302206447c43b9081022e15d1acfa369d3
causeloven <thigo.pro@gmail.com>
2026-04-16 15:54:59 +0300

    RansomNote383

---
+++ src/HashMap/RansomNote383.java
@@ -0,0 +1,29 @@
+package HashMap;
+
+import java.util.HashMap;
+
+class RansomNote383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> mag = new HashMap<>();
+
+        for (char c : magazine.toCharArray()) {
+            mag.put(c, mag.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+
+            if (!mag.containsKey(c)) {
+                return false;
+            }
+
+            mag.put(c, mag.getOrDefault(c, 0) - 1);
+
+            if (mag.get(c) < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
