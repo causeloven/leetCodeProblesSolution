7c27001b661924996ebc055d4b05248837de312b
causeloven <thigo.pro@gmail.com>
2026-04-16 15:44:04 +0300

    ValidAnagram242

--- src/GasStation/Main.java
+++ src/GasStation/Main.java
@@ -2,14 +2,8 @@ package GasStation;
 
 public class Main {
     public static void main(String[] args) {
-        ValidAnagram242 validAnagram242 = new ValidAnagram242();
 
-        String s = "anagram";
-        String t = "nagaram";
-
-        validAnagram242.isAnagram(s,t);
-
-        System.out.println();
 
     }
 }
+
---
+++ src/HashSet/IntersectionofTwoArrays349.java
@@ -0,0 +1,29 @@
+package HashSet;
+
+import java.util.HashSet;
+
+class IntersectionofTwoArrays349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> n1 = new HashSet<>();
+        HashSet<Integer> answer = new HashSet<>();
+
+        for (int num : nums1) {
+            n1.add(num);
+        }
+
+        for (int num : nums2) {
+            if (n1.contains(num)) {
+                answer.add(num);
+            }
+        }
+
+        int[] result = new int[answer.size()];
+        int index = 0;
+        for (int num : answer) {
+            result[index] = num;
+            index++;
+        }
+
+        return result;
+    }
+}
