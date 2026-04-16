d8a9e14988eaee40d6f90571feb499b912ff19e9
causeloven <thigo.pro@gmail.com>
2026-04-17 01:37:03 +0300

    FirstUniqueCharacterinaString387

---
+++ src/Collections/HashMap/FirstUniqueCharacterinaString387.java
@@ -0,0 +1,22 @@
+package Collections.HashMap;
+
+import java.util.HashMap;
+
+class FirstUniqueCharacterinaString387 {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (hashMap.get(c) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
--- src/GasStation/Main.java
+++ src/GasStation/Main.java
@@ -1,48 +1,9 @@
 package GasStation;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> salaries = Arrays.asList(50000, 120000, 80000, 150000, 90000);
-
-        //я не разбираюсь в лямбда выражениях
-        List<Integer> goodSalaries = salaries.stream().filter(s -> s > 100000).collect(Collectors.toList());
-        List<Double> goodSalaries2 = salaries.stream().map(s -> s * 0.87).collect(Collectors.toList());
-        List<Double> goodSalaries3 = salaries.stream().filter(s -> s >= 90000)
-                .map(s -> s * 0.87).sorted((a,b) -> b.compareTo(a)).
-                collect(Collectors.toList());
-
-        System.out.println(goodSalaries);
-        System.out.println(goodSalaries2);
-        System.out.println(goodSalaries3);
-
-        List<String> cars = Arrays.asList(
-                "Audi", "BMW", "Mercedes", // 1 страница
-                "Lexus", "Toyota", "Honda", // 2 страница
-                "Ford", "Chevrolet", "Dodge", // 3 страница
-                "Kia" // 4 страница (остаток)
-        );
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите страницу с машинами: ");
-        long page = sc.nextLong();
-        sc.close();
-        page--;
-        long skip = 3 * page;
-        long limit = 3;
-
-        List<String> carsPage = cars.stream().skip(skip).
-                limit(limit).collect(Collectors.toList());
-
-        System.out.println(carsPage);
-
-        List<Integer> expenses = Arrays.asList(45000, 150000, 80000, 300000);
-        int totalExpenses = expenses.stream().reduce(0, (acc, x) -> acc+x);
-        System.out.println(totalExpenses);
 
     }
 }
