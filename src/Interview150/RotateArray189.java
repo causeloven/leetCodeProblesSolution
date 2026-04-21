d599f96ac2debe3ffd700e47d402a2fe43c0fbbe
causeloven <thigo.pro@gmail.com>
2026-04-21 13:27:53 +0300

    Interview150 - 6 - RotateArray189

---
+++ src/Interview150/RotateArray189.java
@@ -0,0 +1,23 @@
+package Interview150;
+
+public class RotateArray189 {
+    public void rotate(int[] nums, int k) {
+        rot(0,nums.length-1, nums);
+        rot(0, k-1, nums);
+        rot(k, nums.length-1, nums );
+
+    }
+
+    private void rot(int start, int end, int[] nums){
+        int temp = 0;
+        while(start < end){
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+}
