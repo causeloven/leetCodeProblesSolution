994d1b9f5dc3eb1d2022194aceb7a47040bbbe3b
causeloven <thigo.pro@gmail.com>
2026-04-21 17:54:53 +0300

    Interview150 - 9 - JumpGame55

---
+++ src/Interview150/JumpGame55.java
@@ -0,0 +1,20 @@
+package Interview150;
+
+public class JumpGame55 {
+    public boolean canJump(int[] nums) {
+        int reachable = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            if(i > reachable){
+                return false;
+            }
+
+            reachable =  Math.max(reachable, i + nums[i]);
+
+            if(reachable >= nums.length - 1){
+                return true;
+            }
+        }
+        return true;
+    }
+}
