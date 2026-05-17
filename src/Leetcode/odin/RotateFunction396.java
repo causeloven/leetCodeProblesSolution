37cf5016e1e34c2b07f3d424d111aa8088745642
causeloven <thigo.pro@gmail.com>
2026-05-17 21:13:06 +0300

    Interview150 - 39 - RotateFunction396

---
+++ src/Leetcode/odin/RotateFunction396.java
@@ -0,0 +1,33 @@
+package Leetcode.odin;
+
+import java.util.Arrays;
+
+public class RotateFunction396 {
+    public int maxRotateFunction(int[] nums) {
+        int total = 0;
+
+        for(int i : nums){
+            total += i;
+        }
+
+        int f0 = 0;
+        for(int i = 0; i < nums.length; i++){
+            f0 += nums[i] * i;
+        }
+
+        int max = f0;
+        int prev = f0;
+
+        for(int k = 1; k < nums.length; k++){
+
+            int fk = prev + total - nums.length * nums[nums.length - k];
+
+            if(fk > max){
+                max = fk;
+            }
+            prev = fk;
+        }
+
+        return max;
+    }
+}
