c394b3b877d50351c9b547f40fd89b4e966092cb
causeloven <Thigo.pro@gmail.com>
2026-06-01 17:42:18 +0300

    MinimizeMaximumPairSuminArray1877

---
+++ src/Leetcode/june/MinimizeMaximumPairSuminArray1877.java
@@ -0,0 +1,24 @@
+package Leetcode.june;
+
+import java.util.Arrays;
+
+public class MinimizeMaximumPairSuminArray1877 {
+    public int minPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int maxPair = Integer.MIN_VALUE;
+        int i = 0;
+        int j = nums.length-1;
+
+        while(i<j){
+            int pair = nums[i] + nums[j];
+            if(maxPair < pair){
+                maxPair = pair;
+            }
+
+            i++; j--;
+        }
+
+        return maxPair;
+    }
+}
