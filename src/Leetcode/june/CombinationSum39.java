fce57c40e92b6a4305641996fb0c316f75636a2b
causeloven <Thigo.pro@gmail.com>
2026-06-14 16:28:23 +0300

    CombinationSum39

---
+++ src/Leetcode/june/CombinationSum39.java
@@ -0,0 +1,33 @@
+package Leetcode.june;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum39 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, target,0, new ArrayList<>(),result);
+
+        return result;
+
+    }
+
+    private void backtrack(int[] candidates, int remaining, int start, List<Integer> current,
+                           List<List<Integer>> result){
+        if (remaining == 0){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        if(remaining < 0) return;
+
+        for(int i = start; i < candidates.length; i++ ){
+            if(candidates[i] > remaining) break;
+            current.add(candidates[i]);
+            backtrack(candidates, remaining-candidates[i],i,current, result);
+            current.remove(current.size()-1);
+        }
+    }
+}
