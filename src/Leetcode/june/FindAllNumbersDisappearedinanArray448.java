d4a1aafbafc77bd57c68ef79e03e6d208e568ed0
causeloven <Thigo.pro@gmail.com>
2026-06-15 18:58:56 +0300

    FindAllNumbersDisappearedinanArray448

---
+++ src/Leetcode/june/FindAllNumbersDisappearedinanArray448.java
@@ -0,0 +1,31 @@
+package Leetcode.june;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class FindAllNumbersDisappearedinanArray448 {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] > 0){
+                nums[index] = - nums[index];
+            }
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
+                result.add(i+1);
+            }
+        }
+
+        return result;
+
+
+    }
+
+
+}
