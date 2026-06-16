43cdedfe01d5af5da486178b131b2c6fc5d19ab8
causeloven <Thigo.pro@gmail.com>
2026-06-16 19:52:42 +0300

    DetermineifStringHalvesAreAlike1704

--- src/Leetcode/june/Main.java
+++ src/Leetcode/june/Main.java
@@ -1,9 +1,20 @@
 package Leetcode.june;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-        LongestPalindrome409 longestPalindrome409 = new LongestPalindrome409();
+        CombinationSum39 combinationSum39 = new CombinationSum39();
 
-        System.out.println(longestPalindrome409.longestPalindrome("abccccdd"));
+        int[] candies = {2,3,6,7};
+        int target = 7;
+
+        List<List<Integer>> result = combinationSum39.combinationSum(candies, target);
+
+        for(List<Integer> list : result){
+            System.out.println(list);
+        }
+
     }
 }
