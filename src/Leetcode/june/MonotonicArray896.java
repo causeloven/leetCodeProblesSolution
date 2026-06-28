fcadb3fb8f0da95dc591aaa40d2b9f5f07dbff6d
causeloven <Thigo.pro@gmail.com>
2026-06-28 16:48:34 +0300

    MonotonicArray896

---
+++ src/Leetcode/june/MonotonicArray896.java
@@ -0,0 +1,21 @@
+package Leetcode.june;
+
+public class MonotonicArray896 {
+    public boolean isMonotonic(int[] nums) {
+
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] > nums[i+1]){
+                isIncreasing = false;
+            }
+            if(nums[i] < nums[i+1]){
+                isDecreasing = false;
+            }
+        }
+
+        return isDecreasing || isIncreasing;
+    }
+
+
+}
---
+++ src/Leetcode/june/SignoftheProductofanArray1822.java
@@ -0,0 +1,18 @@
+package Leetcode.june;
+
+public class SignoftheProductofanArray1822 {
+    public int arraySign(int[] nums) {
+        int result = 1;
+
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] == 0){
+                return 0;
+            }
+            if (nums[i] < 0) {
+                result *= -1;
+            }
+        }
+
+        return result;
+    }
+}
