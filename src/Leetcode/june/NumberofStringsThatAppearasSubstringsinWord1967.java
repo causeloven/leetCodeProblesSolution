6b6f39cb825f822ef2090db046a9a6dbfa0c6e1f
causeloven <Thigo.pro@gmail.com>
2026-06-29 11:43:38 +0300

    NumberofStringsThatAppearasSubstringsinWord1967

---
+++ src/Leetcode/june/NumberofStringsThatAppearasSubstringsinWord1967.java
@@ -0,0 +1,16 @@
+package Leetcode.june;
+
+import java.util.HashSet;
+
+public class NumberofStringsThatAppearasSubstringsinWord1967 {
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        for(String i : patterns){
+            if(word.contains(i)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
