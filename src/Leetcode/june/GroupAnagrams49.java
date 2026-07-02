cabcbbc5a5a9c7e06d3228e8f661df7d70ce86dd
causeloven <Thigo.pro@gmail.com>
2026-07-02 15:19:48 +0300

    GroupAnagrams49

---
+++ src/Leetcode/june/GroupAnagrams49.java
@@ -0,0 +1,20 @@
+package Leetcode.june;
+
+import java.util.*;
+
+public class GroupAnagrams49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s : strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(s);
+            map.put(key, list);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
