da72aacc5890ea9274adea15d838e7b2afae71a9
causeloven <Thigo.pro@gmail.com>
2026-07-09 20:28:01 +0300

    MaxConsecutiveOnesIII1004

---
+++ src/Leetcode/july04/MaxConsecutiveOnesIII1004.java
@@ -0,0 +1,20 @@
+package Leetcode.july04;
+
+public class MaxConsecutiveOnesIII1004 {
+    public int longestOnes(int[] nums, int k) {
+        int zeroCount = 0;
+        int left = 0;
+        int maxLen = 0;
+
+        for(int right = 0; right < nums.length; right++){
+            if(nums[right] == 0) zeroCount++;
+            while(zeroCount > k){
+                if(nums[left] == 0) zeroCount--;
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+}
