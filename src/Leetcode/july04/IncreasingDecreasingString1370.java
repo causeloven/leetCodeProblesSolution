3e7a7ab33be87c6f09938b77334cfcb95d0b0efa
causeloven <Thigo.pro@gmail.com>
2026-07-19 12:54:20 +0300

    IncreasingDecreasingString1370

---
+++ src/Leetcode/july04/IncreasingDecreasingString1370.java
@@ -0,0 +1,30 @@
+package Leetcode.july04;
+
+public class IncreasingDecreasingString1370 {
+    public String sortString(String s) {
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()){
+            freq[c-'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(sb.length() < s.length()){
+            for(int i = 0; i < 26;i++){
+                if(freq[i] > 0){
+                    sb.append((char)('a' + i));
+                    freq[i]--;
+                }
+            }
+            for(int i = 25; i >= 0; i--){
+                if(freq[i] > 0){
+                    sb.append((char)('a' + i));
+                    freq[i]--;
+                }
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+}
