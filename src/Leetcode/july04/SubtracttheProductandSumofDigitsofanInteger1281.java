630c6e87fc6fe94664931fb190ff3d224cd5fa4a
causeloven <Thigo.pro@gmail.com>
2026-07-21 22:06:55 +0300

    SubtracttheProductandSumofDigitsofanInteger1281

---
+++ src/Leetcode/july04/SubtracttheProductandSumofDigitsofanInteger1281.java
@@ -0,0 +1,18 @@
+package Leetcode.july04;
+
+public class SubtracttheProductandSumofDigitsofanInteger1281 {
+    public int subtractProductAndSum(int n) {
+
+        int a = 1;
+        int b = 0;
+
+        while (n > 0){
+            a *= n % 10;
+            b += n % 10;
+
+            n /= 10;
+        }
+
+        return a - b;
+    }
+}
